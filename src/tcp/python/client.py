e19970d99a97ea5d0c995f5bf54e0efbcb28b87a
Felipe Tiberio Maciel Barbosa <felipe00@A202-17L.imd.local>
2019-06-06 14:41:06 -0300

    Aí não vou ser carregado, estou fazendo algo

--- src/tcp/python/client.py
+++ src/tcp/python/client.py
@@ -3,16 +3,15 @@
 import socket  # Biblioteca para sockets
 
 HOST = "127.0.0.1"  # (localhost)
-PORT_NUMBER = 4324  # Porta usada pelo socket do Servidor
+PORT_NUMBER = 65431 # Porta usada pelo socket do Servidor
 MESSAGE_SIZE = 40 # Quantidade de caracteres que uma mensagem pode transmitir  
 
-
-with socket.socket( socket.AF_INET, socket.SOCK_STREAM ) as s:      # Criando o socket do Cliente
-    s.connect( (HOST, PORT_NUMBER) )    # Conectando socket do Cliente ao socket do Servidor
+with socket.socket( socket.AF_INET, socket.SOCK_STREAM ) as socketCliente:      # Criando o socket do Cliente
+    socketCliente.connect( (HOST, PORT_NUMBER) )    # Conectando socket do Cliente ao socket do Servidor
     print("O socket do Cliente está conectado ao socket do Servidor\n")
     while (True):
-        s.send( str.encode( input() ) )  # Enviando mensagem para o socket do Servidor
-        data = s.recv(MESSAGE_SIZE) # Recebendo mensagem do socket do Servidor
+        socketCliente.send( str.encode( input() ) )  # Enviando mensagem para o socket do Servidor
+        data = socketCliente.recv(MESSAGE_SIZE) # Recebendo mensagem do socket do Servidor
         print("Servidor disse: " + data.decode() )
         if not data or data.decode() == "tchau":
             break
--- src/tcp/python/server.py
+++ src/tcp/python/server.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python3
+from datetime import datetime   # Necessario para pegar a hora do sistema 
+import socket                   # Biblioteca para sockets
 
-import socket  # Biblioteca para sockets
-
-HOST = "192.168.7.1"  # (localhost)
-PORT_NUMBER = 4324  # Porta usada pelo socket do Servidor
+HOST = "127.0.0.1"  # (localhost)
+PORT_NUMBER = 65431  # Porta usada pelo socket do Servidor
 MESSAGE_SIZE = 40 # Quantidade de caracteres que uma mensagem pode transmitir  
 
 
-with socket.socket( socket.AF_INET, socket.SOCK_STREAM ) as s:      # Criando o socket do Servidor
-    s.bind( ( HOST, PORT_NUMBER ) )     # Ligando o socket do Servidor a porta PORT_NUMBER
+with socket.socket( socket.AF_INET, socket.SOCK_STREAM ) as socketServidor:      # Criando o socket do Servidor
+
+    socketServidor.bind( ( HOST, PORT_NUMBER ) )     # Ligando o socket do Servidor a porta PORT_NUMBER
     print( "Ligacao entre socket Servidor e porta " +  str(PORT_NUMBER) + " executada com sucesso...\n")
-    s.listen( )     # Colocando o socket do Servidor para escutar as requisições  
+    socketServidor.listen( )     # Colocando o socket do Servidor para escutar as requisições  
     print( "O socket do Servidor está escutando com sucesso...\n")
-    conn, addr = s.accept()      # Retirando da fila de requisições uma requisição
+    conn, addr = socketServidor.accept()      # Retirando da fila de requisições uma requisição
+
     with conn:
         print( "Connected by " + str(addr) + "\n")
         while True:
+            now = datetime.now()
             data = conn.recv(MESSAGE_SIZE)  # Recebendo mensagem do socket do Cliente
             print("Cliente disse:" + data.decode() + "\n")
             if not data or data.decode() == "tchau":
                 conn.send( str.encode( "tchau" ) )
                 break
-            conn.send( str.encode( input() ) ) # Enviando mensagem para socket do Cliente
+            conn.send( str.encode( now.strftime("%H:%M:%S") ) ) # Enviando mensagem para socket do Cliente
