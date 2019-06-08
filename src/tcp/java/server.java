c33284197b5c9182584e37d4b6a6d47e834ddb19
Clenilson Silva <clenilson.silva4@hotmail.com>
2019-06-08 00:15:30 -0300

    Map adicionado ao server
    Server respondendo os clientes conectados e exceção modificada para remover o socket do map caso a conexão com o cliente seja encerrada.

--- src/tcp/java/server.java
+++ src/tcp/java/server.java
@@ -1,65 +1,88 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Date;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 import java.text.SimpleDateFormat;
 
-public class server {
+public class server{
     static int cont = 0;
     static Date tempoLocal;
     static SimpleDateFormat ft = new SimpleDateFormat("HH:mm:ss");
+    static Map <Socket, String> connectedSockets = new HashMap <Socket, String> ();
     
-    public static void main(String[] args) throws Exception {
-        ServerSocket serverSocket = new ServerSocket(12900, 100,
-                InetAddress.getLocalHost());
-        System.out.println("Servidor iniciado em: " + serverSocket);
-
-        while (true) {
-            System.out.println("Agurdando conexão...");
-            System.out.println("");
-
+    public static void main(String[] args) throws Exception{
+        ServerSocket serverSocket = new ServerSocket(12900, 100, InetAddress.getLocalHost());
+        System.out.println("Servidor iniciado em: " + serverSocket.getInetAddress() + " - " + serverSocket.getLocalPort());
+        System.out.println("Agurdando conexão...");
+        System.out.println("");
+        
+        while(true){
             final Socket activeSocket = serverSocket.accept();
 
-            System.out.println("Conexão recebida de  " + activeSocket);
+            System.out.println("Conexão recebida de " + activeSocket);
             Runnable runnable = () -> handleClientRequest(activeSocket);
             new Thread(runnable).start(); // start a new thread
         }
     }
 
-    public static void handleClientRequest(Socket socket) {
-        try {
-            BufferedReader socketReader = null;
-            BufferedWriter socketWriter = null;
-            socketReader = new BufferedReader(new InputStreamReader(
-                    socket.getInputStream()));
-            socketWriter = new BufferedWriter(new OutputStreamWriter(
-                    socket.getOutputStream()));
-
-            String inMsg = null;
-            while ((inMsg = socketReader.readLine()) != null) {
-                tempoLocal = new Date();
-                if(inMsg.equalsIgnoreCase("sair")){
-                    System.out.println("Conexão encerrada pelo cliente");
-                    socketWriter.write("Sair");
+    public static void handleClientRequest(Socket socket){
+        try{
+            BufferedReader socketReader;
+            socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedWriter socketWriter;
+            socketWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            String inMsg, identifier;
+            
+            socketWriter.write("Como gostaria de ser identificado?\n");
+            socketWriter.flush();
+            identifier = socketReader.readLine();
+            tempoLocal = new Date();
+            connectedSockets.put(socket, identifier);
+            String outMsg = "Servidor: mensagem recebida às: " + ft.format(tempoLocal);
+            socketWriter.write(outMsg);
+            socketWriter.write("\n");
+            socketWriter.flush();
+            
+            for(Map.Entry <Socket, String> it : connectedSockets.entrySet()){
+                if(it.getKey() != socket){
+                    socketWriter = new BufferedWriter(new OutputStreamWriter(it.getKey().getOutputStream()));
+                    socketWriter.write(identifier + " entrou na conversa.");
                     socketWriter.write("\n");
                     socketWriter.flush();
-                    break;
                 }
-                System.out.println("Cliente : " + inMsg);
-                
-                String outMsg = "\nMensagem recebida às: " + ft.format(tempoLocal);
+            }
+            
+            while((inMsg = socketReader.readLine()) != null){
+                tempoLocal = new Date();
+                socketWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                outMsg = "Servidor: mensagem recebida às: " + ft.format(tempoLocal);
                 socketWriter.write(outMsg);
                 socketWriter.write("\n");
                 socketWriter.flush();
+                
+                if(inMsg.equalsIgnoreCase("sair")){
+                    System.out.println("Conexão encerrada pelo cliente");
+                    socketWriter.write("Sair\n");
+                    socketWriter.flush();
+                    connectedSockets.remove(socket);
+                    break;
+                }
+                
+                System.out.println(identifier + ": " + inMsg);                
+                
+                for(Map.Entry <Socket, String> it : connectedSockets.entrySet()){
+                    if(it.getKey() != socket){
+                        socketWriter = new BufferedWriter(new OutputStreamWriter(it.getKey().getOutputStream()));
+                        socketWriter.write(identifier + ": " + inMsg);
+                        socketWriter.write("\n");
+                        socketWriter.flush();
+                    }
+                }
             }
             socket.close();
-        } catch (Exception e) {
+        }catch (Exception e){
             System.out.println("\nA conexão foi encerrada inesperadamente pelo Cliente " + "");
             System.out.println("");
+            connectedSockets.remove(socket);
         }
     }
 }
